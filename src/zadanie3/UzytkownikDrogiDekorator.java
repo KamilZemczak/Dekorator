1c57a7358f72c2c71b579d8c96bf50722e5cc777
KamilZemczak <kamilzemb@gmail.com>
2016-10-30 13:18:09 +0100

    Zadanie 1 & 2 - DONE Zadanie 3 - IN PROGRESS (abstract class)

---
+++ src/zadanie3/UzytkownikDrogiDekorator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie3;
+
+/**
+ *
+ * @author Kamil Zemczak
+ */
+public interface UzytkownikDrogiDekorator extends UzytkownikDrogi {
+     public void modify(UzytkownikDrogi uzytkownikDrogi);
+}
